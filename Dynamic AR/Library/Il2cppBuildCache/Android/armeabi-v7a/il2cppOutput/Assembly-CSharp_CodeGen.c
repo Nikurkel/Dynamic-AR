f0830fafb6998b728db8e66cb0f599ee501c8c7d
Nikurkel <66831842+Nikurkel@users.noreply.github.com>
2021-12-06 19:06:33 +0100

    imported Project

---
+++ Dynamic AR/Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,63 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void TrackedImageInfoManager::Awake()
+extern void TrackedImageInfoManager_Awake_mD3485AB1187972C999B6B9B0FF8CEEF882514FF8 (void);
+// 0x00000002 System.Void TrackedImageInfoManager::OnEnable()
+extern void TrackedImageInfoManager_OnEnable_m3E16243E50F1B77410B60040E46B8CF9A0006692 (void);
+// 0x00000003 System.Void TrackedImageInfoManager::OnDisable()
+extern void TrackedImageInfoManager_OnDisable_m0CFD1A40ED52663F0DD6210AC66DC1263C0966CD (void);
+// 0x00000004 System.Void TrackedImageInfoManager::OnTrackedImagesChanged(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void TrackedImageInfoManager_OnTrackedImagesChanged_m3A38DE894818966C29D29E5F913D2BE5BE70C18A (void);
+// 0x00000005 System.Void TrackedImageInfoManager::.ctor()
+extern void TrackedImageInfoManager__ctor_m076CB263EFFEC0C4415691AE6263599B00B7DD57 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	TrackedImageInfoManager_Awake_mD3485AB1187972C999B6B9B0FF8CEEF882514FF8,
+	TrackedImageInfoManager_OnEnable_m3E16243E50F1B77410B60040E46B8CF9A0006692,
+	TrackedImageInfoManager_OnDisable_m0CFD1A40ED52663F0DD6210AC66DC1263C0966CD,
+	TrackedImageInfoManager_OnTrackedImagesChanged_m3A38DE894818966C29D29E5F913D2BE5BE70C18A,
+	TrackedImageInfoManager__ctor_m076CB263EFFEC0C4415691AE6263599B00B7DD57,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	1887,
+	1887,
+	1887,
+	1535,
+	1887,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
